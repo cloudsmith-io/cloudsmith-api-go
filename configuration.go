e8993071a1a54a7e28d8af7779de5865cbe1c846
Chris McEvoy <cmcevoy@cloudsmith.io>
2023-01-24 17:09:26 +0000

    v0.0.21, not v0.0.20

--- configuration.go
+++ configuration.go
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.0.20/go",
+		UserAgent:        "OpenAPI-Generator/0.0.21/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
