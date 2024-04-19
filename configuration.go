e52fa6dd8577c9fb4db2dbe8c61420e5659b7c0d
Bartosz Blizniak <55028730+BartoszBlizniak@users.noreply.github.com>
2024-04-19 12:30:18 +0100

    chore: Update binding for OrgsOpenidConnectCreateExecute (#25)
    * update bindings for OrgsOpenidConnectCreateExecute
    
    * re-add contribution
    
    * chore: updating version number
    
    ---------
    
    Co-authored-by: Domagoj <dkatavic@cloudsmith.io>

--- configuration.go
+++ configuration.go
@@ -3,7 +3,7 @@ Cloudsmith API (v1)
 
 The API to the Cloudsmith Service
 
-API version: 1.392.0
+API version: 1.401.1
 Contact: support@cloudsmith.io
 */
 
@@ -93,7 +93,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.0.34/go",
+		UserAgent:        "OpenAPI-Generator/0.0.35/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
